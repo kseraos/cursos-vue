2c6113d1162117276bc9e704d75f3a1d5bbe8b3f
kristine <kseraos@outlook.com>
2020-12-19 11:13:11 -0400

    cadastro

--- src/components/FormCadastro.vue
+++ src/components/FormCadastro.vue
@@ -1,14 +1,39 @@
 <template>
-   <b-modal id="modal-1" title="BootstrapVue" v-model="abrirModal">
-    <p class="my-4">Hello</p>
+   <b-modal id="modal-1" title="BootstrapVue" v-model="abrirModal" hide-footer>
+     <b-form @submit.prevent="onSubmit">
+        <b-form-group label="Nome:">
+            <b-form-input v-model="form.nome" type="text" required> </b-form-input>
+        </b-form-group>
+        <b-form-group label="URL:">
+            <b-form-input v-model="form.url" type="text" required> </b-form-input>
+      </b-form-group>
+      <b-form-group label="Descrição:">
+            <b-form-input v-model="form.descricao" type="text" required> </b-form-input>
+      </b-form-group>
+    <b-form-group>
+      <input type="file" @change="selecionaCapa">
+    </b-form-group>
+
+    <b-button type="submit" variant="primary">Salvar</b-button>
+     </b-form>
   </b-modal>
 </template>
 
 <script>
+import axios from 'axios' 
+const Compress = require('compress.js')
+
 export default {
     nome: 'FormCadastro',
     data (){
         return{
+            compress: new Compress(),
+            form:{
+                nome:'',
+                url:'',
+                capa: '',
+                descricao:''
+            }
 
         }
     },
@@ -23,6 +48,21 @@ export default {
           
         }
 
+    },
+    methods:{
+        selecionaCapa (evt){
+            const files = [...evt.target.files]
+
+            this.compress.compress(files, {
+                size:4,
+                quality: .75,
+            }).then((data)=> {
+                this.form.capa = data[0].data
+            })
+        },
+        onSubmit (){
+            axios.post('http://laravel-vue-novo.com/api/cursos', this.form)
+        }
     }
 }
 </script>
